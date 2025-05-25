c343e47c6ec9c18e7afe0d95420013727294e762
Jose Villalta <josevillalta@vimasistem.com>
2025-05-25 01:02:40 -0500

    feat: Lugar pozo (provincia, ciudad, parroquia)

--- SAVIAQUA.Core/DTOs/Pozos/PozoMinResponse.cs
+++ SAVIAQUA.Core/DTOs/Pozos/PozoMinResponse.cs
@@ -8,6 +8,11 @@ public class PozoMinResponse
     public int CodigoJunta { get; set; }
     public decimal Latitude { get; set; }
     public decimal Longitude { get; set; }
+    
+    public string Provincia { get; set; }
+    public string Ciudad { get; set; }
+    public string Parroquia { get; set; }
+    
     public DateTime FechaCreacion { get; set; }
     
 }
--- SAVIAQUA.Infraestructure/Queries/PozosQueries.cs
+++ SAVIAQUA.Infraestructure/Queries/PozosQueries.cs
@@ -9,15 +9,30 @@ public static class PozosQueries
                 p.codigo_junta as CodigoJunta,
                 p.latitude,
                 p.longitude,
+                pp.nombre as Provincia,
+                c.nombre as Ciudad,
+                p3.nombre as Parroquia,
                 p.fecha_creacion as FechaCreacion
                 from pozos p
                 inner join juntas j
                 on j.codigo = p.codigo_junta
+                inner join provincias pp
+                on pp.codigo = p.codigo_provincia
+                inner join ciudades c
+                on c.codigo = p.codigo_ciudad
+                inner join parroquias p3
+                on p3.codigo = p.codigo_parroquia
                 order by p.nombre, j.nombre, p.fecha_creacion";
 
     public const string ObtenerTotalPozos = @"select
                 count(*)
                 from pozos p
                 inner join juntas j
-                on j.codigo = p.codigo_junta";
+                on j.codigo = p.codigo_junta
+                inner join provincias pp
+                on pp.codigo = p.codigo_provincia
+                inner join ciudades c
+                on c.codigo = p.codigo_ciudad
+                inner join parroquias p3
+                on p3.codigo = p.codigo_parroquia";
 }
